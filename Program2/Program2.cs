2e8319b0352f187709fe46e67cfaa1b99d018ce9
Adeyemi Funto <feekah@Adeyemis-MacBook-Air.local>
2022-08-11 16:33:02 +0100

    testing microphone

---
+++ Program2/Program2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goal
+{
+    class Prog
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter aany of these operators: +, -, *, and / ");
+            string numoperator = Console.ReadLine();
+
+
+            Console.WriteLine(operation(num1, num2, numoperator));
+
+
+
+
+        }
+
+
+
+        static double operation(double num1, double num2, string numoperator)
+        {
+            double result = 0;
+
+            if (numoperator == "+")
+            {
+                result = num1 + num2;
+            }
+
+            else if (numoperator == "-")
+            {
+                result = num1 - num2;
+            }
+
+            else if (numoperator == "*")
+            {
+                result = num1 * num2;
+            }
+
+            else if (numoperator == "/")
+            {
+                result = num1 / num2;
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+            return result;
+        }
+
+    }
+}
+
+
+string secretword = "love";
+string guess = "";
+int guesscount = 0;
+int guesslimit = 4;
+bool outofguesses = false;
+
+while (guess != secretword && !outofguesses)
+{
+    if (guesscount < guesslimit)
+    {
+        Console.WriteLine("Please enter a secret word");
+        guess = Console.ReadLine();
+        guesscount++;
+
+    }
+
+    else
+    {
+        outofguesses = true;
+    }
+
+}
+if (outofguesses)
+{
+    Console.WriteLine("You lose!");
+}
+else
+{
+    Console.WriteLine("You win!");
+}
---
+++ firstcsharp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goal
+{
+    class Prog
+    {
+        static void Main(string[] args)
+        {
+            int[] lucky = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for(int i = 0; i< lucky.Length; i++)
+            {
+                Console.WriteLine(lucky[i]);
+            }
+
+            
+        }
+
+        static string Daycheck(int DayNum)
+        {
+            string dayName;
+
+            switch (DayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+
+                case 1:
+                    dayName = "Monday";
+                    break;
+
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+
+                case 4:
+                    dayName = "Thursday";
+                    break;
+
+                case 5:
+                    dayName = "Friday";
+                    break;
+
+                case 6:
+                    dayName = "Saturday";
+                    break;
+
+                default:
+                    dayName = "Invalid day number";
+                    break;
+
+            }
+            return dayName;
+
+        }
+
+
+    }
+
+}
